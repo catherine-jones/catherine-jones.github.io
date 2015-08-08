4deaf773594b44829bfb0fdaf01bd7c2f45425f5
Catherine Jones <catherine.jones41@gmail.com>
2015-08-08 14:02:25 +1000

    feat: set up email notification

--- send_mail.php
+++ send_mail.php
@@ -1,17 +1,17 @@
 <?php
-	$to = "jdrendon@imaginacionweb.net"; /*Your Email*/
-	$subject = "Messsage from Mycv Html"; /*Issue*/
-	$date = date ("l, F jS, Y"); 
-	$time = date ("h:i A"); 	
-	$msg="
-	Name: $_REQUEST[Name]
-	Email: $_REQUEST[Email]
-	
-	Message sent from website on date  $date, hour: $time.\n
+  $to = "cath@bitgirl101.com"; /*Your Email*/
+  $subject = "Message from Resume Site"; /*Issue*/
+  $date = date ("l, F jS, Y");
+  $time = date ("h:i A");
+  $msg="
+  Name: $_REQUEST[Name]
+  Email: $_REQUEST[Email]
 
-	Message:
-	$_REQUEST[message]";
+  Message sent from website on date  $date, hour: $time.\n
 
-	mail($to, $subject, $msg, "From: $_REQUEST[Email]");
-	echo "Thank you for your message";
+  Message:
+  $_REQUEST[message]";
+
+  mail($to, $subject, $msg, "From: $_REQUEST[Email]");
+  echo "Thank you for your message";
 ?>
